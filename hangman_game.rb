e4b81ec82e792de0bb362490db1ea09b8239cc47
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-24 22:53:11 -0500

    adds displaying of hung man

--- hangman_game.rb
+++ hangman_game.rb
@@ -1,17 +1,22 @@
 class Hangman
   def initialize
     @secret_word = 'testing'.split('') # select_word_from_list.split
-    @hanged_status = Array.new(3, [])
     @incorrect_guesses = []
     @correct_guesses = Array.new(@secret_word.length, '_')
   end
 
   def select_word_from_list; end
 
-  def display_playfield; end
+  def display_playfield
+    puts hanged_status
+    puts @correct_guesses.to_s
+    puts '----------------------------'
+    puts @incorrect_guesses.to_s
+  end
 
   def guess_handler(guesses)
     guesses.each do |letter|
+      next if win? || lose?
       correct_guess?(letter) ? update_correct_guesses(letter) : update_incorrect_guesses(letter)
     end
   end
@@ -21,24 +26,33 @@ class Hangman
   end
 
   def update_incorrect_guesses(letter)
-    next if win? || lose?
     @incorrect_guesses << letter
     puts "Guesses #{@guesses}"
     @incorrect_guesses
   end
 
   def update_correct_guesses(letter)
-    next if win? || lose?
     @secret_word.each_with_index do |secret_letter, index|
       @correct_guesses[index] = letter if letter == secret_letter
     end
     puts "Correct Guesses: #{@correct_guesses}"
   end
 
-  def update_hanged_status
-    hanged_man = { head: ' O', rarm: '\\', larm: ' /',
-                   torso: " |\n |", rleg: '/ ', lleg: '\\' }
-    # use @incorrect_guesses.length to calculate amount of limbs
+  def hanged_status
+    case @incorrect_guesses.length
+    when 1
+      ['O']
+    when 2
+      [[' O'], [' |'], [' |']]
+    when 3
+      [[' O'], ['\\|'], [' |']]
+    when 4
+      [[' O'], ['\\|/'], [' |']]
+    when 5
+      [[' O'], ['\\|/'], [' |'], ["\/"]]
+    when 6
+      [[' O'], ['\\|/'], [' |'], ["\/ \\"]]
+    end
   end
 
   def win?
