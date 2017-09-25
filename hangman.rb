da30fe1fd553f4418eb61f8697586ce48b6e31e6
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-25 18:00:42 -0500

    Saves files, almost

--- hangman.rb
+++ hangman.rb
@@ -1,24 +1,40 @@
+require 'json'
 class GameEngine
   require './hangman_game.rb'
   def initialize
     @hangman = Hangman.new
-    menu
   end
 
-  def menu
-    # Give options for:
-    #   1. Start a new game
-    #   2. Exit
+  def show_menu
+    puts 'Main Menu'
+    puts '1. Play Game'
+    puts '2. Save Game'
+    puts '3. Load Game'
+    menu_options(gets.chomp.to_i)
+  end
+
+  def menu_options(selection)
+    case selection
+    when 1
+      main_loop
+    when 2
+      save_game(to_json)
+    when 3
+      load_game
+    else
+      puts 'Invalid Selection'
+      show_menu
+    end
   end
 
   def main_loop
-    until win? || lose?
-      puts "\n\n"
-      @hangman.display_playfield
-      puts 'Make a guess!'
-      @hangman.guess_handler(recieve_input)
-      puts "\n\n"
-    end
+    # until win? || lose?
+    puts "\n\n"
+    @hangman.display_playfield
+    puts 'Make a guess!'
+    @hangman.guess_handler(recieve_input)
+    puts "\n\n"
+    # end
     win? ? win_message : lose_message
   end
 
@@ -56,6 +72,16 @@ class GameEngine
   def lose_message
     puts 'You lost!'
     @hangman.display_playfield
+  end
+
+  def to_json
+    JSON.generate({hangman: @hangman})
+  end
+
+  def save_game(string)
+    File.open('saved.json', 'w') do |game_file|
+      game_file.write(string)
+    end
   end
 end
 
